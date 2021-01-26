c339ca390595c54d5a7851163d9e1b1e2c2d2837
Plain Jane <jane99hsu@gmail.com>
2021-01-25 22:05:38 -0800

    UPDATE actions.js
    UPDATED action to UPDATE_FILTERED_PALETTES.  WAS UPDATE_UPVOTED_PALETTES.
    - Functionality will include a filter for "most recent", "most upvoted", or "most liked by day/week" therefore action needs to be generic.
    
    DELETED action UPDATE_SEARCHED_PALETTE
    - Searched palettes do not need to be saved

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,17 +1,14 @@
 // Actions define the types of events that can be emitted to update state. 
 
 // actions for the home page
-// store the data retrieved by Apollo for the most up-voted palettes in this global state
-export const UPDATE_UPVOTED_PALETTES = "UPDATE_UPVOTED_PALETTES";
+// store the data retrieved by Apollo for the user-filtered palettes in this global state
+export const UPDATE_FILTERED_PALETTES = "UPDATE_FILTERED_PALETTES";
 
 // store the data retrieved by Apollo for palette-of-the-day in this global state
 export const UPDATE_DAILY_PALETTE = "UPDATE_DAILY_PALETTE";
 
 // store the data retrieved by Apollo for palette-of-the-week in this global state
 export const UPDATE_WEEKLY_PALETTE = "UPDATE_WEEKLY_PALETTE";
-
-// store the data retrieved by Apollo for searched palettes in this global state
-export const UPDATE_SEARCHED_PALETTE = "UPDATE_SEARCHED_PALETTE";
 
 // action for the user page and create palette page
 // store the data retrieved by Apollo for myPalettes in this global state
