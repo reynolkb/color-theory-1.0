ac15bba9cb33ed9ab8c781283e6ff4c876fed2c3
Plain Jane <jane99hsu@gmail.com>
2021-01-26 22:20:43 -0800

    UPDATE App.js - add import of ApolloClient - create functionality to connect to GraphQL - add <ApolloProvider> component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// import functionality to make requests to GraphQL server 
+import { useQuery } from '@apollo/react-hooks';
+
+// import queries 
+import { QUERY_PALETTES } from '../utils/queries';
+
+
 import { fakePalettes } from '../const/colors'
 
 import Palette from '../components/Palette';
@@ -7,12 +14,29 @@ import Sidebar from '../components/Sidebar';
 
 const Home = () => {
 
-    return(
-        <>
+    // load the Home component & execute the query for the Palette data
+    // use loading property to:
+    // --- indicate that the request isn't done just yet
+    // --- conditionally render data based on whether or not there is data to even display
+    // data returned from the server stored in the destructured data property
+    const { loading, data } = useQuery(QUERY_PALETTES);
+
+    // get Palette data out of the query's response with optional chaining
+    // if data exists, store it in the palette constant we just created
+    // if data is undefined, save empty array 
+    const palettes = data?.palettes || [];
+    console.log(palettes);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <main>
             <div>
                 {
                     fakePalettes.map((palette, i) => (
-                        <Palette key={i} palette={palette}/>
+                        <Palette key={i} palette={palette} />
                     ))
                 }
             </div>
@@ -20,8 +44,8 @@ const Home = () => {
             <div>
                 <Sidebar />
             </div>
-        </>
+        </main>
     );
 };
 
-export default  Home;
+export default Home;
