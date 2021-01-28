412addf80fcd663e950c65793f1742039aa12382
Plain Jane <jane99hsu@gmail.com>
2021-01-27 19:24:45 -0800

    Added JSX and useQuery functionality to Home page
    UPDATE Home.js page
    - add useQuery functionality on QUERY_PALETTES. commented out until GraphQL functionality is complete.
    - add palettes const to contain data from QUERY_PALETTES.  commented out until GraphQL functionality is complete.
    - add temporary palettes const to contain fakePalettes data
    - add JSX to bring in useQuery data to Homepage.  commented out useQuery data until GraphQL functionality is complete.
    
    UPDATE Palette.js model
    - add 'saves' property to model to store users who have saved the palette
    - add 'saveCount' virtual to calculate number of users who have saved the palette to their user profile
    
    UPDATE colors.js fake data
    - revise palette to contain username key instead of user key
    - revise palette to contain upvoteCount key instead of likes key

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PALETTES } from '../utils/queries';
 
 // this is Cat's test data
-// import { fakePalettes } from '../const/colors'
+import { fakePalettes } from '../const/colors'
 
 import Palette from '../components/Palette';
 import Sidebar from '../components/Sidebar';
@@ -19,27 +19,33 @@ const Home = () => {
     // --- indicate that the request isn't done just yet
     // --- conditionally render data based on whether or not there is data to even display
     // data returned from the server stored in the destructured data property
-    const { loading, data } = useQuery(QUERY_PALETTES);
-
+    // const { loading, data } = useQuery(QUERY_PALETTES);
+    
     // get Palette data out of the query's response with optional chaining
     // if data exists, store it in the palette constant we just created
     // if data is undefined, save empty array 
-    const palettes = data?.palettes || [];
-    console.log(palettes);
+    // const palettes = data?.palettes || [];
+    const palettes = fakePalettes;
+    // console.log(palettes);
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
 
     return (
         <main>
             <div className='global-wrapper'>
                 <div className='home-palette-wrapper'>
-                    {
+                    {/* cat's styling palettes */}
+                    {/* {
                         fakePalettes.map((palette, i) => (
                             <Palette key={i} palette={palette} />
                         ))
-                    }
+                    } */}
+
+                    {/* {loading ? (
+                        <div>Loading...</div>
+                    ) : ( */}
+                            {/* // Once query is complete and loading is undefined, pass palettes array to <PaletteList> component as props */}
+                            <Palette palettes={palettes} />
+                        {/* )} */}
                 </div>
 
                 <div className='sidebar-wrapper'>
