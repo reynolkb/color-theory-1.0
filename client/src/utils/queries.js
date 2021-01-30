d8cbd3b01e2c0ccb58661440b4c19fa15d9edd59
ginomaglaqueucla <68166953+ginomaglaqueucla@users.noreply.github.com>
2021-01-30 10:55:52 -0800

    Gino/tag frontend (#42)
    * added tags to front end query
    
    * populate tag names to frontend

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -13,6 +13,10 @@ export const QUERY_PALETTES = gql`
 			accent3
 			username
 			upvoteCount
+			tags {
+				_id
+				name
+			}
 			upvotes {
 				username
 			}
@@ -35,7 +39,21 @@ export const QUERY_PALETTE = gql`
             secondary
             accent1
             accent2
-            accent3
+			accent3
+			username
+			upvoteCount
+			tags {
+				_id
+				name
+			}
+			upvotes {
+				username
+			}
+			createdAt
+			saveCount
+			saves {
+				username
+			}
 		}
 	}
 `;
