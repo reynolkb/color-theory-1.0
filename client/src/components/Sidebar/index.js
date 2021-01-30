cde9a4b60c3436d2d6b15292f9218e4a0d2e2eb9
Plain Jane <jane99hsu@gmail.com>
2021-01-30 02:15:13 -0800

    ATTEMPT to create filter functionality by date
    PUSH code to get group debugging help
    
    UPDATE Sidebar index.js
    - add code to compare dates for filtering
    
    UPDATE dateFormat.js
    - add functionality to convert createdAt timestamps to Date objects

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -3,46 +3,70 @@ import React from 'react';
 import DailyPalette from '../DailyPalette';
 import WeeklyPalette from '../WeeklyPalette';
 
+import { dateFormat, convertToInt } from '../../utils/dateFormat';
+
 // this is Cat's test data
 // import { dailyPalette, weeklyPalette } from '../../const/colors';
 
 const Sidebar = ({ palettes }) => {
 
     console.log(palettes);
-    // for dummy data
-    // const daily = dailyPalette;
-    // const weekly = weeklyPalette;
+
+    // get today's date
+    const today = new Date();
+    console.log(today);
+    console.log(typeof today);
+    console.log("This is today's date: " + today.toDateString());
+
+    // create yesterday's date
+    const todayMinusOne = today.getDate() - 1;
+    const yesterday = today.setDate(todayMinusOne);
+    console.log(yesterday);
+    console.log(typeof yesterday);
+    // console.log("This is yesterday's date: " + yesterday.toDateString());
+    // const yesterdayInMilli = yesterday.getTime();
+    // console.log("This is yesterday's date: " + yesterdayInMilli);
     
+    // for palette-of-the-day
+    // loop over palette array starting at the beginning since array is in descending order
+    for (let i = 0; i < 3; i++) {
+    // for (let i = 0; i < palettes.length; i++) {
+
+        let palettesToday = [];
+
+        // const date = dateCompareFormat(palettes[i].createdAt);
+        const createdAt = palettes[i].createdAt;
+        console.log(typeof createdAt);
+        const date = convertToInt(createdAt);
+        console.log(typeof date);
+        console.log("This is palette " + i + " with a date of: " + date);
+
+        if (date > yesterday) {
+            console.log("this was not created today.");
+        } else {
+            console.log("this was created today");
+        }
+
+
+
+
+    // if (palettes[i].createdAt
+    }
+
+
+    // stop once it hits a date from yesterday
+
     const daily = palettes[1];
+
+
+
     const weekly = palettes[2];
 
-    const createdDate = palettes[1].createdAt;
-    console.log("number of milliseconds: " + createdDate);
-    console.log(typeof createdDate);
-
-    const milliseconds = parseInt(createdDate);
-    console.log(milliseconds);
-
-    const convert = new Date(milliseconds);
-    console.log(convert);
 
 
-    // convert received date to string
-    // const dateToString = Date(createdDate);
-    // console.log(dateToString);
-    // change received date to Date object
-    // const dateObject = new Date(dateToString);
-    // console.log(dateObject);
-
-    // const todayFormatted = dateFormat(today);
 
     // console.log(todayFormatted);
 
-    // if (palettes[0].createdAt === today) {
-    //     console.log("we are equal");
-    // } else {
-    //     console.log("we are not equal");
-    // }
 
 
     // const dailyFilter = () => {
@@ -50,21 +74,17 @@ const Sidebar = ({ palettes }) => {
     //     const today = Date();
     //     console.log(today);
 
-    //     for (let i = 0; i < palettes.length; i++) {
-    //         let dailyPal = {};
-    //         if (palettes[i].createdAt
-    //     }
     // }
 
     return (
         <div>
             <h3 className='sidebar-title'>Trending Today</h3>
             {/* palette of the day  */}
-            <DailyPalette palette={daily}/>
+            <DailyPalette palette={daily} />
 
             <h3 className='sidebar-title'>Trending this Week</h3>
             {/* palette of the week  */}
-            <WeeklyPalette palette={weekly}/>
+            <WeeklyPalette palette={weekly} />
         </div>
     )
 };
--- client/src/utils/dateFormat.js
+++ client/src/utils/dateFormat.js
@@ -1,12 +1,22 @@
 export function dateFormat(milliseconds) {
 
     const stringToInt = parseInt(milliseconds);
-    console.log("number of milliseconds: " + stringToInt);
-    console.log(typeof stringToInt);
+    // console.log("number of milliseconds: " + stringToInt);
+    // console.log(typeof stringToInt);
 
     const date = new Date(stringToInt);
-    console.log(date);
+    // console.log(date);
 
     return date.toDateString();
+
+}
+
+export function convertToInt(milliseconds) {
+
+    const stringToInt = parseInt(milliseconds);
+
+    const date = new Date(stringToInt);
+
+    return date;
 
 }
