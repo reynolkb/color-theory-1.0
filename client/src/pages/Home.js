4feda4580709e1dd2682ab076130a3f1036cf373
Plain Jane <jane99hsu@gmail.com>
2021-01-31 23:44:34 -0800

    ADD Global State and IndexedDB store
    UPDATE App.js
    - add <Provider> component to encompass <Nav> and <main>
    
    UPDATE Home.js
    - add Global State using Redux
    - add IndexedDB functionality to store Global State
    - code is not working
    - pushing to save progress for debugging tomorrow

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// for Global State using Redux, use React-Redux hook
+import { useSelector, useDispatch } from 'react-redux';
+import { UPDATE_PALETTES } from '../utils/actions';
+import { idbPromise } from '../utils/helpers';
 
 // import functionality to make requests to GraphQL server 
 import { useQuery } from '@apollo/react-hooks';
@@ -12,16 +17,56 @@ import Sidebar from '../components/Sidebar';
 
 const Home = () => {
 
+    // create Global state
+    const state = useSelector(state => state);
+    console.log(state);
+    const dispatch = useDispatch();
+
+    // destructure from global state object
+    // const { palettes } = state;
+    // console.log(palettes);
+
     // load the Home component & execute the query for the Palette data
     // data returned from the server stored in the destructured data property
     // query for main content
-    const { loading, data } = useQuery(QUERY_PALETTES);
+    // const { loading, data } = useQuery(QUERY_PALETTES);
+    // console.log(data);
+    const { loading, data: dbPaletteData } = useQuery(QUERY_PALETTES);
+    console.log(dbPaletteData);
+
+    useEffect(() => {
+        // if dbPaletteData exists or has changed, run dispatch to update Global State
+        if (dbPaletteData) {
+            dispatch({
+                type: UPDATE_PALETTES,
+                palettes: dbPaletteData.palettes
+            });
+            console.log("data received");
+
+            // put the data into the palettes store
+            dbPaletteData.palettes.forEach(palette => {
+                idbPromise('palettes', 'put', palette);
+                console.log("data delivered");
+            });
+
+        } else if (!loading) {
+            // get the data from IndexedDb
+            idbPromise('palettes', 'get').then(palettes => {
+                dispatch({
+                    type: UPDATE_PALETTES,
+                    palettes: palettes
+                });
+            });
+            console.log("i got data from indexedDb");
+        }
+    }, [dbPaletteData, loading, dispatch]);
 
     // get Palette data out of the query's response with optional chaining
     // if data exists, store it in the palette constant we just created
     // if data is undefined, save empty array 
     // constant for main content
-    const palettes = data?.palettes || [];
+    // const palettes = data?.palettes || [];
+    // console.log(palettes);
 
     // palettes.length && console.log(typeof palettes[0]._id);
 
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,21 +2,20 @@
 // list of the possible actions we can perform to update state
 import {
     UPDATE_PALETTES,
-    UPDATE_DAILY_PALETTE,
-    UPDATE_WEEKLY_PALETTE,
-    UPDATE_MY_PALETTES,
-    UPDATE_FAVORITE_PALETTES,
-    UPDATE_CURRENT_PALETTE,
+    // UPDATE_DAILY_PALETTE,
+    // UPDATE_WEEKLY_PALETTE,
+    // UPDATE_MY_PALETTES,
+    // UPDATE_FAVORITE_PALETTES,
+    // UPDATE_CURRENT_PALETTE,
 } from "./actions";
 
 const initialState = {
     palettes: [],
-    dailyPalette: '',
-    weeklyPalette: '',
-    myPalette: [],
-    favorites: [],
-    currentPalette: '',
-    menuOpen: false
+    // dailyPalette: '',
+    // weeklyPalette: '',
+    // myPalette: [],
+    // favorites: [],
+    // currentPalette: ''
 };
 
 export const reducer = (state = initialState, action) => {
@@ -27,41 +26,35 @@ export const reducer = (state = initialState, action) => {
                 palettes: [...action.palettes]
             };
 
-        case UPDATE_DAILY_PALETTE:
-            return {
-                ...state,
-                dailyPalette: action.dailyPalette
-            };
+        // case UPDATE_DAILY_PALETTE:
+        //     return {
+        //         ...state,
+        //         dailyPalette: action.dailyPalette
+        //     };
 
-        case UPDATE_WEEKLY_PALETTE:
-            return {
-                ...state,
-                weeklyPalette: action.weeklyPalette
-            };
+        // case UPDATE_WEEKLY_PALETTE:
+        //     return {
+        //         ...state,
+        //         weeklyPalette: action.weeklyPalette
+        //     };
 
-        case UPDATE_MY_PALETTES:
-            return {
-                ...state,
-                myPalette: [...state.myPalette, ...action.palette]
-            };
+        // case UPDATE_MY_PALETTES:
+        //     return {
+        //         ...state,
+        //         myPalette: [...state.myPalette, ...action.palette]
+        //     };
 
-        case UPDATE_FAVORITE_PALETTES:
-            return {
-                ...state,
-                favorites: [...state.favorites, ...action.palette]
-            };
+        // case UPDATE_FAVORITE_PALETTES:
+        //     return {
+        //         ...state,
+        //         favorites: [...state.favorites, ...action.palette]
+        //     };
 
-        case UPDATE_CURRENT_PALETTE:
-            return {
-                ...state,
-                currentPalette: currentPalette
-            };
-
-        case TOGGLE_MENU:
-            return { 
-                ...state,
-                menuOpen: !state.menuOpen
-            };
+        // case UPDATE_CURRENT_PALETTE:
+        //     return {
+        //         ...state,
+        //         currentPalette: currentPalette
+        //     };
 
         default:
             return state;
