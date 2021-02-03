c7b448b6fdeb39a3656072a5e188e3a171d5f518
ginomaglaque <ginomaglaque@gmail.com>
2021-02-02 19:26:34 -0800

    temp seed only locally with new connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/color-theory', {
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project3', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/color-theory', {
+  // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project3', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
