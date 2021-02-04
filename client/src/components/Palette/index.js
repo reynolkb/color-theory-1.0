5cfefa1abfa7602dcc3ab2d3dd3f8787b233941e
cat - lin <plummorgan@gmail.com>
2021-02-03 19:54:15 -0800

    Did media queries to all pages and components, palette, likes, weekly, and daily

--- client/src/components/Palette/index.js
+++ client/src/components/Palette/index.js
@@ -14,32 +14,30 @@ import { useSelector, useDispatch } from 'react-redux';
 const Palette = ({ palettes }) => {
 
     return (
-        <div>
+        <>
             {palettes &&
                 palettes.map(palette => (
                 // filterPalettes().map(palette => (
-                    <div key={palette._id}>
-                        <div className='palette-wrapper'>
-                            <h3 className='palette-title'>{palette.title}</h3>
-                            <p>by <Link to={`/user/${palette.username}`}>{palette.username}</Link> on {dateFormat(palette.createdAt)}</p>
-                            <div>
-                                {/* to palette details page */}
-                                <Link to={`/details/${palette._id}`}>
-                                    <Colors palette={palette} />
-                                </Link>
-                            </div>
-                            <SocialButtons upvoteCount={palette.upvoteCount} saveCount={palette.saveCount} paletteId={palette._id} />
-                            <div>
-                                <h5 className='palette-description'>Description:</h5>
-                                <p>
-                                    {palette.description}
-                                </p>
-                            </div>
+                    <div key={palette._id} className='palette-wrapper'>
+                        <h3 className='palette-title'>{palette.title}</h3>
+                        <p>by <Link to={`/user/${palette.username}`}>{palette.username}</Link> on {dateFormat(palette.createdAt)}</p>
+                        <div>
+                            {/* to palette details page */}
+                            <Link to={`/details/${palette._id}`}>
+                                <Colors palette={palette} />
+                            </Link>
+                        </div>
+                        <SocialButtons upvoteCount={palette.upvoteCount} saveCount={palette.saveCount} paletteId={palette._id} />
+                        <div>
+                            <h5 className='palette-description'>Description:</h5>
+                            <p>
+                                {palette.description}
+                            </p>
                         </div>
                     </div>
                 ))
             }
-        </div>
+        </>
 
     )
 }
